6565f01dd8d4e23b970f1f66b5cc4613fa71e5fa
Oscar <oscar@rabble.se>
2021-05-27 12:21:35 +0200

    Specify cache-first strategy for service worker. Make sw registration code inline and cache manifest

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,16 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    'ember-service-worker': {
+      registrationStrategy: 'inline',
+      versionStrategy: 'every-build',
+    },
+    'esw-index': {
+      strategy: 'cache-first',
+    },
+    'asset-cache': {
+      include: ['assets/**/*', 'manifest.webmanifest'],
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
