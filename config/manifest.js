2729145a415472a2846a9e815c8f4318b320a435
Oscar <oscar@rabble.se>
2021-06-11 13:38:27 +0200

    Update app icon with helmet version and add splash screens

--- config/manifest.js
+++ config/manifest.js
@@ -12,25 +12,43 @@ module.exports = function (/* environment, appConfig */) {
     scope: '/',
     display: 'standalone',
     background_color: '#dfdfdf',
-    theme_color: '#dfdfdf',
+    theme_color: '#1063ff',
     orientation: 'portrait',
     icons: [
       {
         src: '/assets/icons/favicon.ico',
         targets: ['favicon'],
       },
-      {
-        src: '/assets/icons/apple-touch-icon.png',
-        sizes: '180x180',
+      ...[120, 152, 180].map((size) => ({
+        src: `/assets/icons/apple-touch-icon-${size}x${size}.png`,
+        sizes: `${size}x${size}`,
         targets: ['apple'],
+      })),
+      {
+        src: '/assets/icons/safari-pinned-tab.svg',
+        safariPinnedTabColor: '#1063ff',
+        targets: ['safari-pinned-tab'],
       },
-      ...[72, 96, 128, 144, 152, 192, 384, 512].map((size) => ({
+      {
+        src: '/assets/icons/maskable_icon_x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
+        targets: ['manifest'],
+      },
+      ...[72, 96, 144, 192, 256, 384, 512].map((size) => ({
         src: `/assets/icons/icon-${size}x${size}.png`,
         sizes: `${size}x${size}`,
+        targets: ['manifest'],
       })),
+      {
+        src: '/assets/icons/mstile-150x150.png',
+        element: 'square150x150logo',
+        targets: ['ms'],
+      },
     ],
     ms: {
-      tileColor: '#fff',
+      tileColor: '#2d89ef',
     },
   };
 };
